0afd3798e7679410775433bdd256a1ccefd4c59c
Octavia <octavia.efraim@gmail.com>
2023-07-28 10:44:07 +0200

    TP 3: data binding

--- app/src/main/java/com/example/tp_twitter/MainActivity.kt
+++ app/src/main/java/com/example/tp_twitter/MainActivity.kt
@@ -2,32 +2,36 @@ package com.example.tp_twitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.tp_twitter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit  var myView : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        myView = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // au clic sur le bouton
+        myView.btnLogin.setOnClickListener() {
+            onSubmitLogin()
+        }
     }
 
-    fun onSubmitLogin(view: View) {
+    fun onSubmitLogin() {
         // On vide le message d'erreur
-        findViewById<TextView>(R.id.tvError).text = ""
+        myView.tvError.text = ""
 
         // Liste de messages d'erreur. Vide par défaut
         var errors: ArrayList<String> = ArrayList<String>()
 
         // On vérifie les champs :
         // - email
-        val edtLogin = findViewById<EditText>(R.id.edtLogin)
+        val edtLogin = myView.edtLogin
         if (edtLogin.text.isNullOrEmpty() || edtLogin.text.toString().length < 4) {
             errors.add("Email incorrect")
         }
 
         // - password
-        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+        val edtPassword = myView.edtPassword
         if (edtPassword.text.isNullOrEmpty() || edtPassword.text.toString().length < 6) {
             errors.add("Password incorrect")
         }
@@ -37,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             for (error in errors) {
                 message += "\n" + error
             }
-            findViewById<TextView>(R.id.tvError).text = message
+            myView.tvError.text = message
         }
     }
 }
